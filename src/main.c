837b7f423c93828d81c29fd15dcf9fcb392a0dd2
Tareq Dawoudiah <td310705@hotmail.com>
2020-06-15 22:35:44 -0400

    resolving a conflict

--- src/main.c
+++ src/main.c
@@ -1,22 +1,117 @@
+#ifndef STDINT_H
+#define STDINT_H
+#include <stdint.h>
+#endif
+
+#ifndef SDTBOOL_H
+#define STDBOOL_H
+#include <stdbool.h>
+#endif
+
+#ifndef STDLIB_H
+#define STDLIB_H
+#include <stdlib.h>
+#endif
+
 #ifndef STDIO_H
 #define STDIO_H
-
 #include <stdio.h>
+#endif
 
-#endif /* STDIO_H */
+#ifndef STDDEF_H
+#define STDDEF_H
+#include <stddef.h>
+#endif
 
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAZE_H
+#define MAZE_H
+#include "maze.h"
+#endif
 
-#include <main.h>
+#ifndef TILE_DEAFULT_H
+#define TILE_DEAFULT_H
+#include "tile_default.h"
+#endif
 
-#endif /* MAIN_H */
+#ifndef TILE_H
+#define TILE_H
+#include "tile.h"
+#endif
 
+#ifndef PRINTER_H
+#define PRINTER_H
+#include "printer.h"
+#endif
+
+void init_test_maze_1(maze_t* maze){
+    init_maze(5, 4, maze);
+    cell_t c;
+
+    c = get_cell(maze, 0, 0);
+    c.east  = false;
+    c.start = true;
+    set_cell(c, 0, 0, maze);
+
+    c = get_cell(maze, 2, 0);
+    c.east  = false;
+    c.south = false;
+    c.west  = false;
+    set_cell(c, 2, 0, maze);
+
+    c = get_cell(maze, 4, 0);
+    c.south = false;
+    set_cell(c, 4, 0, maze);
+
+    c = get_cell(maze, 0, 1);
+    c.east  = false;
+    c.south = false;
+    set_cell(c, 0, 1, maze);
+
+    c = get_cell(maze, 2, 1);
+    c.east  = false;
+    set_cell(c, 2, 1, maze);
+
+    c = get_cell(maze, 4, 1);
+    c.south = false;
+    c.west  = false;
+    set_cell(c, 4, 1, maze);
+
+    c = get_cell(maze, 0, 2);
+    c.east  = false;
+    c.south = false;
+    set_cell(c, 0, 2, maze);
+
+    c = get_cell(maze, 2, 2);
+    c.east  = false;
+    c.south = false;
+    set_cell(c, 2, 2, maze);
+
+    c = get_cell(maze, 2, 3);
+    c.east  = false;
+    c.west  = false;
+    set_cell(c, 2, 3, maze);
+
+    c = get_cell(maze, 4, 3);
+    c.west  = false;
+    c.end = true;
+    set_cell(c, 4, 3, maze);
+
+}
 
 int main(int argc, char* argv[]){
-    printf("There are %d arguments.\nThey are the following:\n", argc);
-    for(int i = 0; i<argc; i++){
-        printf("%d: %s\n", i, argv[i]);
-    }
+
+    //Initializing test maze
+    maze_t myMaze;
+    maze_t *pmyMaze = &myMaze;
+    init_test_maze_1(pmyMaze);
+
+    //Initializing tile set
+    tile_set_t myTileSet;
+    tile_set_t *pmyTileSet = &myTileSet;
+    myTileSet = hedge_set;
+
+    //Testing my print function
+    print(pmyMaze, pmyTileSet , stdout);
+
     return 0;
 }
