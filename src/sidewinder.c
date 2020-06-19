afdb10b79236b392b0615ab80bc9b65db724e04c
Marwan Ghalib <ghalib.marwan@gmail.com>
2020-06-19 15:02:34 -0400

    Adding more functionality to sidewinder.c
    Still not complete

--- src/sidewinder.c
+++ src/sidewinder.c
@@ -33,11 +33,105 @@
 #include <time.h>
 #endif
 
+// Used for adding and keeping track of cells in the "run" set
+struct CELL_COORDINATES
+{
+    int x;
+    int y;
+};
+
+// Initialize a "run" set
+struct CELL_COORDINATES* initialize_run_set(int maze_width) {
+
+    struct CELL_COORDINATES *run_set = (struct CELL_COORDINATES*)calloc(maze_width, sizeof(struct CELL_COORDINATES));
+
+    return run_set;
+}
+
+// Add a cell to the "run" set and randomly decide whether to go east or not
+bool add_to_run_set(struct CELL_COORDINATES *run_set, int x, int y) {
+
+    struct CELL_COORDINATES cell_to_be_added;
+    cell_to_be_added.x = x;
+    cell_to_be_added.y = y;
+
+    // add to run set
+    run_set = &cell_to_be_added;
+    run_set++;
+
+    // randomly decide whether to go east or not
+    bool go_east = rand() & 1;
+
+    return go_east;
+}
+
+// Empty the "run" set
+void clear_run_set(struct CELL_COORDINATES *run_set) {
+
+    free(run_set);
+
+}
+
+void repeated_procedures(struct CELL_COORDINATES run_set[], int current_column, int current_row, maze_t* maze) {
+
+    bool go_east = add_to_run_set(run_set, current_column, current_row);
+    cell_t new_cell;
+    cell_t cell;
+    int random_number;
+
+    cell = get_cell(maze, current_column, current_row);
+
+
+
+    if (go_east) {
+        // carve your way east
+        cell.east = 0;
+
+        // new cell is now the current cell
+        current_column++;
+        new_cell = get_cell(maze, current_column, current_row);
+
+
+        // CHECK FOR SOMETHING BEFORE YOU REPEATING THE ABOVE PROCEDURES
+
+        // Recursion?, can we do realloc??
+        repeated_procedures(run_set, current_column, current_row, maze);
+
+    } else {
+        // randomly choose a cell from the "run" set
+        random_number = rand() % maze->width;
+        struct CELL_COORDINATES random_cell_coordinates = run_set[random_number];
+
+        cell_t random_cell = get_cell(maze, random_cell_coordinates.x, random_cell_coordinates.y);
+
+        // carve North
+        random_cell.north = 0;
+
+        // empty "run" set
+        clear_run_set(run_set);
+
+        // set next cell in row to current cell
+
+        // CHECK FOR SOMETHING BEFORE YOU REPEATING THE ABOVE PROCEDURES
+
+        // Recursion?, can we do realloc??
+        repeated_procedures(run_set, current_column, current_row, maze);
+    }
+
+}
+
+// Generates a maze based on the Sidewinder algorithm
 void gen_sidewinder(maze_t* maze) {
 
-    int current_row = 0;
+    srand((unsigned)time(NULL));
 
-    // clear entire 1st row (i.e. set cell_t.east to 0)
+    // start at 0,0
+    int current_row = 0;
+    int current_column = 0;
+
+    cell_t new_cell;
+
+    // clear entire 1st row (i.e. set cell_t.east to 0 for each cell in that row)
     cell_t cell;
     for (int i = 0; i < maze->width; i++) {
         cell = get_cell(maze, i, current_row);
@@ -48,29 +142,14 @@ void gen_sidewinder(maze_t* maze) {
     // move on to next row
     current_row++;
 
-    // initializ a "run" set to empty
-    cell_t run_set[maze->width];
+    // initialize a "run" set to empty
+    struct CELL_COORDINATES *run_set = initialize_run_set(maze->width);
 
-    // add current cell to "run" set
-    cell = get_cell(maze, 0, current_row);
-    run_set[0] = cell;
-
-    // randomly decide whether to go east or not
-    srand((unsigned)time(NULL));
-    bool go_east = rand() & 1;
-
-    // if you decided to go east
-    if (go_east) {
-        // new cell is now the current cell
-
-
-    } else {
-
-    }
-
+    repeated_procedures(run_set, current_column, current_row, maze);
 
 }
 
+// -----------------------------------------------------------------------
 
 int main()
 {
