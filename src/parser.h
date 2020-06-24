b3171293e80b42852eb444d7172f9129a60727c3
Tareq Dawoudiah <td310705@hotmail.com>
2020-06-23 23:36:48 -0400

    Added comments to the parser header file

--- src/parser.h
+++ src/parser.h
@@ -13,4 +13,11 @@
 #include "tile.h"
 #endif
 
+/**
+This function will take in a maze object, look at it's file pointer that contains 
+a text file with a maze, and populate the maze object data.
+
+After running parser, you can now take this maze object and pass it to the printer to get printed.
+*/
+
 void parse_maze(maze_t *my_maze);
